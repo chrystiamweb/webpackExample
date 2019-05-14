642579cb2f3f87594249264da9f3a72d3f645498
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-05-14 15:05:25 +0000

    testing how to load a js file, import a class and call on index

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import Life from './life';
+ 
+var life = new Life;
+console.log(life.awesome());
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
 const webpack = require('webpack');
+ 
 module.exports = {
-context: path.resolve(__dirname, 'src'),
-entry: {
-app: './app.js',
-},
-output: {
-path: path.resolve(__dirname, 'dist'),
-filename: '[name].bundle.js',
-},
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        app: './app.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        publicPath: '/assets'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'src')
+    }
 };
